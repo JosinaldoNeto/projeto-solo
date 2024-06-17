a854064c3fdbb4b97107bfe3fc75d2146f42220e
JosinaldoNeto <jpgneto@yahoo.com.br>
2024-06-16 22:30:15 -0300

    att

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+
+
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+
+btnAvancar.addEventListener("click", function () {
+    
+    if (cartaoAtual === cartoes.length - 1) return;
+    
+    const cartaoSelecionado = document.querySelector(".selecionado");
+
+    cartaoAtual++;
+    cartoes[cartaoAtual].classList.add("selecionado")
+    cartaoSelecionado.classList.remove("selecionado");
+
+});
+
+
+btnVoltar.addEventListener("click", function () {
+    if(cartaoAtual === 0) return;
+
+
+    const cartaoSelecionado =document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add("selecionado");
+
+})
+
+
+
+
